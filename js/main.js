24f340e3eed5549f697d69c651fea1501a277fae
Irinka04 <69346145+Irinka04@users.noreply.github.com>
2021-03-23 14:43:16 +0300

    Add files via upload
    Учебный проект GLO Академии . С добавлением JS скриптовю

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+// card
+const buttonCard = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+// Получаем для закрытия модального окна крестик
+const modalСlose = document.querySelector('.modal-close');
+// переменная которая открывает опен модал
+const openModal = function () {
+	modalCart.classList.add('show');
+};
+// переменная которая закрывает close модал
+const closeModal = function () {
+	modalCart.classList.remove('show');
+};
+// навешаем слушатель событий
+buttonCard.addEventListener('click', openModal);
+modalСlose.addEventListener('click', closeModal);
+// scrol link 
+// получаю ссылки 
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', function (event) {
+			event.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+
+			});
+		});
+	}
+}
